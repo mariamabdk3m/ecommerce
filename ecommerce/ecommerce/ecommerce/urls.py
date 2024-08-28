48f5c9adf65bc07cb66c06f01eba5c7c5076997a
Mariam Abdalmagied <mariamabdalmageid@gmail.com>
2024-08-28 16:21:27 +0300

    Delete ecommerce/ecommerce directory

--- ecommerce/ecommerce/ecommerce/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('tracking_system/', include('tracking_system.urls')),  # Ensure this line is correctly included
-]
--- ecommerce/ecommerce/tracking_system/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrackingSystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tracking_system'
